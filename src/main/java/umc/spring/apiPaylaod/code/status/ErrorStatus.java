b027f0244e1ed28f07983746084e3280fbcfbfe5
dokyung-kang <kangdk02@naver.com>
2024-11-22 15:19:43 +0900

    fix : STORE_NOT_FOUND 충돌 에러
    merge 후 STORE_NOT_FOUND 중복되어 충돌나는 에러 해결
    #8

--- src/main/java/umc/spring/apiPaylaod/code/status/ErrorStatus.java
+++ src/main/java/umc/spring/apiPaylaod/code/status/ErrorStatus.java
@@ -39,12 +39,6 @@ public enum ErrorStatus implements BaseErrorCode {
     // 지역 관련 에러
     REGION_NOT_FOUND(HttpStatus.BAD_REQUEST, "REGION4001", "지역이 없습니다."),
 
-
-
-
-    // 가게 에러
-    STORE_NOT_FOUND(HttpStatus.BAD_REQUEST, "STORE4001", "가게가 없습니다."),
-
     // 예시,,,
     ARTICLE_NOT_FOUND(HttpStatus.NOT_FOUND, "ARTICLE4001", "게시글이 없습니다."),
 
