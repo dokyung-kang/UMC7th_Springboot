c309152e026c6657a08a51aa40278a6d3c8626c6
dokyung-kang <kangdk02@naver.com>
2024-11-24 21:18:22 +0900

    feat : 내가 작성한 리뷰 목록
    #15

--- src/main/java/umc/spring/service/MemberService/MemberCommandServiceImpl.java
+++ src/main/java/umc/spring/service/MemberService/MemberCommandServiceImpl.java
@@ -1,6 +1,8 @@
 package umc.spring.service.MemberService;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import umc.spring.apiPaylaod.code.status.ErrorStatus;
@@ -9,9 +11,11 @@ import umc.spring.converter.MemberConverter;
 import umc.spring.converter.MemberPreferConverter;
 import umc.spring.domain.FoodCategory;
 import umc.spring.domain.Member;
+import umc.spring.domain.Review;
 import umc.spring.domain.mapping.MemberPrefer;
 import umc.spring.repository.FoodCategoryRepository;
 import umc.spring.repository.MemberRepository.MemberRepository;
+import umc.spring.repository.ReviewRepository.ReviewRepository;
 import umc.spring.service.FoodCategoryService.FoodCategoryService;
 import umc.spring.web.dto.MemberRequestDTO;
 
@@ -23,7 +27,7 @@ import java.util.stream.Collectors;
 public class MemberCommandServiceImpl implements MemberCommandService{
 
     private final MemberRepository memberRepository;
-
+    private final ReviewRepository reviewRepository;
     private final FoodCategoryService foodCategoryService;
 
     @Override
@@ -41,5 +45,13 @@ public class MemberCommandServiceImpl implements MemberCommandService{
         memberPreferList.forEach(memberPrefer -> {memberPrefer.setMember(newMember);});
 
         return memberRepository.save(newMember);
+    }
+
+    // 내가 작성한 리뷰 목록 조회
+    public Page<Review> getReviewList(Long memberId, Integer page) {
+
+        Member member = memberRepository.findById(memberId).get();
+        Page<Review> MemberPage = reviewRepository.findAllByMember(member, PageRequest.of(page, 10));
+        return MemberPage;
     }
 }
