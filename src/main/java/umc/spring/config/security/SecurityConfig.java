ff28bc525cb1be26b92e3e23ceb3d4b147f9c6c6
dokyung-kang <kangdk02@naver.com>
2024-12-27 00:47:53 +0900

    fix: 배포 시 에러 해결 시도1
    #42

--- src/main/java/umc/spring/config/security/SecurityConfig.java
+++ src/main/java/umc/spring/config/security/SecurityConfig.java
@@ -16,12 +16,14 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
+                .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests((requests) -> requests
                         // Swagger UI 관련 경로에 접근 허용
-                        .requestMatchers("/swagger-ui/**", "/v3/api-docs/**", "/swagger-resources/**", "/webjars/**", "/api/**").permitAll()
+                        .requestMatchers("/swagger-ui/**", "/v3/api-docs/**", "/swagger-resources/**", "/webjars/**").permitAll()
                         // 기타 경로 설정
                         .requestMatchers("/", "/home", "/signup", "/members/signup", "/css/**").permitAll()
                         .requestMatchers("/admin/**").hasRole("ADMIN")
+                        .requestMatchers("/api/**").permitAll()
                         .anyRequest().authenticated()
                 )
                 .formLogin((form) -> form
